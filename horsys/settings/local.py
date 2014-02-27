5ac4bf85a3976ae49d00e5170f6130a2f53394b6
Garrett Listi <glisti14@gmail.com>
2014-02-26 18:08:28 -0600

    create setting module

---
+++ horsys/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+WSGI_APPLICATION = 'horsys.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'horsys.db'),
+    }
+}
+
+# INSTALLED_APPS += (
+#     'dashboard',
+#     'profiles',
+# )
---
+++ horsys/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+WSGI_APPLICATION = 'horsys.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'horsysdb',
+        'HOST': '/opt/bitnami/postgresql',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'Ir5Lmc67m2'
+    }
+}
+
+# INSTALLED_APPS += ('dashboard', 'profiles')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# TEMPLATE_DIRS = (
+#     os.path.join(BASE_DIR, 'dashboard/templates'),
+#     os.path.join(BASE_DIR, 'profiles/templates'),
