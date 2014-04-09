d93bb99318fe96028f789168b2c04076ce8eb2a7
austin <austin@austin-Ubuntu32.(none)>
2014-04-09 12:25:13 -0500

    UI looks Djanky, but the shift am and shift pm stuff is now in there.

--- schedule/models.py
+++ schedule/models.py
@@ -3,23 +3,67 @@ from django.core.urlresolvers import reverse
 from django.utils.encoding import smart_text
 from django.utils import timezone
 import datetime
+from datetime import date
 
 # Create your models here.
 
 class Log(models.Model):
-	date = models.DateTimeField('date published')
 	desc = models.CharField(max_length=100)
-	details = models.CharField(max_length=1000)
+	injuries = models.TextField()
+	beh_changes = models.TextField()
+	horse_name = models.CharField(max_length=100)
+	tasks = models.TextField()
+	feed_change = models.TextField()
+	comments = models.TextField()
+	date = models.DateField()
 	auth = models.CharField(max_length=100)
-	horse = models.CharField(max_length=100)
 
 	def is_today(self):
-		return self.date >= timezone.now() - datetime.timedelta(days=1)
+		return self.date >= date.today()
 
 	def is_yesterday(self):
-		return ( (self.date >= timezone.now() - datetime.timedelta(days=2)) and not (self.date >= timezone.now() - datetime.timedelta(days=1)))
+		t = date.today()
+		n = abs(t - self.date)
+		return n.days is 1
 
 	def __unicode__(self):
 		return self.desc
 	def get_absolute_url(self):
 		return reverse('logs.views.log_detail', args=[str(self.id)])
+
+
+class ShiftAM(models.Model):
+	feeder_name = models.CharField(max_length=100)
+	date = models.DateField()
+	turnout = models.BooleanField()
+	close_haytrap = models.BooleanField()
+	clean_stalls = models.BooleanField()
+	tidy_barn = models.BooleanField()
+	clean_area = models.BooleanField()
+
+	def __unicode__(self):
+		return self.feeder_name
+	def is_today(self):
+		return self.date >= date.today()
+	def get_absolute_url(self):
+		return reverse('shiftAM.views.shift_detail', args=[str(self.id)])
+	def title(self):
+		return self.feeder_name + "  " + self.date.isoformat()
+
+
+class ShiftPM(models.Model):
+	feeder_name = models.CharField(max_length=100)
+	date = models.DateField()
+	turnout = models.BooleanField()
+	open_haytrap = models.BooleanField()
+	tidy_barn = models.BooleanField()
+	clean_area = models.BooleanField()
+	lights_off = models.BooleanField()
+	def __unicode__(self):
+		return self.feeder_name
+	def is_today(self):
+		return self.date >= date.today()
+	def get_absolute_url(self):
+		return reverse('shiftPM.views.shift_detail', args=[str(self.id)])
+	def title(self):
+		return self.feeder_name + "  " + self.date.isoformat()
--- schedule/urls.py
+++ schedule/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import patterns, include, url
 from schedule.views import LogList, LogCreate, LogDetail, \
-                         LogUpdate, LogDelete
+                         LogUpdate, LogDelete, ShiftAMList, ShiftAMCreate, ShiftAMDetail, \
+                         ShiftAMUpdate, ShiftAMDelete, ShiftPMCreate, ShiftPMDetail, ShiftPMUpdate, ShiftPMDelete
 urlpatterns = patterns('',
     url(r'^$', LogList.as_view(), name='log_list'),
     url(r'^add/$', LogCreate.as_view(), name='log_create'),
     url(r'^(?P<pk>\d+)/$', LogDetail.as_view(), name='log_detail'),
     url(r'^(?P<pk>\d+)/update/$', LogUpdate.as_view(), name='log_update'),
     url(r'^(?P<pk>\d+)/delete/$', LogDelete.as_view(), name='log_delete'),
+    url(r'^shiftAM/$', ShiftAMList.as_view(), name='shiftAM_list'),
+    url(r'^shiftAM/add/$', ShiftAMCreate.as_view(), name='shiftAM_create'),
+    url(r'^shiftAM/(?P<pk>\d+)/$', ShiftAMDetail.as_view(), name='shiftAM_detail'),
+    url(r'^shiftAM/(?P<pk>\d+)/update/$', ShiftAMUpdate.as_view(), name='shiftAM_update'),
+    url(r'^shiftAM/(?P<pk>\d+)/delete/$', ShiftAMDelete.as_view(), name='shiftAM_delete'),
+    url(r'^shiftPM/add/$', ShiftPMCreate.as_view(), name='shiftPM_create'),
+    url(r'^shiftPM/(?P<pk>\d+)/$', ShiftPMDetail.as_view(), name='shiftPM_detail'),
+    url(r'^shiftPM/(?P<pk>\d+)/update/$', ShiftPMUpdate.as_view(), name='shiftPM_update'),
+    url(r'^shiftPM/(?P<pk>\d+)/delete/$', ShiftPMDelete.as_view(), name='shiftPM_delete'),
 )
