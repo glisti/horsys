76b4b4404d7beb637a66d4e8b53fdcb58bdbda5e
Garrett Listi <glisti14@gmail.com>
2014-04-14 17:25:47 -0500

    refactor schedule and move log to horse

--- horses/forms.py
+++ horses/forms.py
@@ -4,7 +4,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Button
 from crispy_forms.bootstrap import FormActions
 
-from horses.models import Horse, MedicalRecord, Task, Note
+from horses.models import Horse, MedicalRecord, Task, Log
 
 form_actions = FormActions(
     Submit('save_changes', 'Save'),
@@ -47,14 +47,14 @@ class TaskForm(forms.ModelForm):
         model = Task
         exclude = ['created','modified']
 
-class NoteForm(forms.ModelForm):
+class LogForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        super(NoteForm, self).__init__(*args, **kwargs)
+        super(LogForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_method = 'post'
         self.helper.form_action = ''
         self.helper.form_class  = 'well form-vertical'
         self.helper.layout.append(form_actions)
     class Meta:
-        model = Note
+        model = Log
         exclude = ['created','modified']
--- schedule/forms.py
+++ schedule/forms.py
@@ -2,19 +2,7 @@ from django import forms
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from schedule.models import Log, ShiftAM, ShiftPM
-
-class LogForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(LogForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper(self)
-        self.helper.form_method = 'post'
-        self.helper.form_action = ''
-        self.helper.form_class = 'well'
-        self.helper.add_input(Submit('submit','submit'))
-
-    class Meta:
-        model = Log
+from schedule.models import Shift
 
 class ShiftAMForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -26,7 +14,8 @@ class ShiftAMForm(forms.ModelForm):
         self.helper.add_input(Submit('submit','submit'))
 
     class Meta:
-        model = ShiftAM
+        model = Shift
+        exclude = ['lights_off',]
 
 class ShiftPMForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -38,4 +27,5 @@ class ShiftPMForm(forms.ModelForm):
         self.helper.add_input(Submit('submit','submit'))
 
     class Meta:
-        model = ShiftPM
+        model = Shift
+        exclude = []
