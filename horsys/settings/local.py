3ae0a71730604d551f6d3c6cd4aa5c0b0767bc7f
Garrett Listi <glisti14@gmail.com>
2014-04-22 13:37:21 -0500

    install inpectional registration with bare templates

--- horsys/settings/local.py
+++ horsys/settings/local.py
@@ -10,9 +10,17 @@ WSGI_APPLICATION = 'horsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'horsys.db'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'horsys',
+        'HOST': 'localhost',
+        'PORT': '',
+        'USER': '',
+        'PASSWORD': ''
     }
+    # 'default_lite': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'horsys.db'),
+    # }
 }
 
 INSTALLED_APPS += (
@@ -22,4 +30,14 @@ INSTALLED_APPS += (
     'autofixture',
     'dev',
     'core',
+    'registration',
 )
+
+REGISTRATION_SUPPLEMENT_CLASS = None
+ACCOUNT_ACTIVATION_DAYS       = 7
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'glisti14@gmail.com'
+EMAIL_HOST_PASSWORD = 'tamu2014'
