d97d4d80b5b4d51d08c13eb09c16f584a6214618
Garrett Listi <glisti14@gmail.com>
2014-04-27 16:43:26 -0500

    email settings fixed

--- horsys/settings/local.py
+++ horsys/settings/local.py
@@ -40,5 +40,5 @@ ACCOUNT_ACTIVATION_DAYS       = 7
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '*'
-EMAIL_HOST_PASSWORD = '*'
+EMAIL_HOST_USER = 'horsyscalendar@gmail.com'
+EMAIL_HOST_PASSWORD = 'calendarhorsy'
